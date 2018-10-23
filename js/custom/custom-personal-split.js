847c2a69dd08b114cfabd9ba7223ee49b7b54693
Sparks <zaharus000@mail.ru>
2018-10-23 17:41:01 +0700

    Update custom-personal-split.js

--- js/custom/custom-personal-split.js
+++ js/custom/custom-personal-split.js
@@ -73,7 +73,7 @@
 		// Type text
 		
 		var typed = new Typed('#typed-1', {
-			strings: ["Hello.", "I'm a web developer.", "Stack of technologies:", "PHP7, Laravel, Webpack, Bootrstrap", "Ne perdez pas de temps", "Écrivez maintenant", "B pf B pf B pf ", "B trrrrrrr B B pf B B B", "...", "Time is money!", "I am waiting.", 'Brrrrr', "ok.", "by.", "We're no strangers to love", "You know the rules and so do I", "A full commitment's what I'm thinking of", "You wouldn't get this from any other guy", "I just wanna tell you how I'm feeling", "Gotta make you understand", "Never gonna give you up, never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry, never gonna say goodbye", "Never gonna tell a lie and hurt you"],
+			strings: ["Hello.", "Technology stack:", "PHP7, Laravel, Webpack, Bootrstrap", "JS/CSS/HTML, Vue.js basics", "B pf B pf B pf ", "B trrrrrrr B B pf B B B", "...", 'Brrrrr', "ok.", "We're no strangers to love", "You know the rules and so do I", "A full commitment's what I'm thinking of", "You wouldn't get this from any other guy", "I just wanna tell you how I'm feeling", "Gotta make you understand", "Never gonna give you up, never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry, never gonna say goodbye", "Never gonna tell a lie and hurt you"],
 			typeSpeed:45,
 			backSpeed:0,
 			startDelay:200,
